3fc78c35a7876b7d242ac341563d36f2e1367cdf
chandanacr-13 <114760882+chandanacr-13@users.noreply.github.com>
2025-01-10 21:19:17 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+if(isset($_SESSION['uid'])){
+    echo "";
+    }else{
+    header('location: ../index.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <style>
+        body
+        {
+        background-image:url('../images/1.jpg');   
+        background-repeat: no-repeat;
+        background-size: cover;
+        }
+    </style>
+</head>
+<body>
+    <?php include('header.php'); ?>
+    <div align='center' style="font-weight: bold;font-family:'Times New Roman', Times, serif"><br><br><br><br>
+        <h1>This is Quickpick Courier Management Service</h1>
+        <h4>The fastest courier service of India</h4><br><br>
+    </div>
+</body>
+</html>
